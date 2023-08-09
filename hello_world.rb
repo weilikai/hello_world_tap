b77c844605253fb32c2094e274b76a8e33754721
weilikai01 <weilikai01@baidu.com>
2023-08-09 21:29:18 +0800

    init

--- hello_world.rb
+++ hello_world.rb
@@ -1,8 +1,8 @@
 class HelloWorld < Formula
   desc "Command-line program to print 'Hello, World!'"
-  homepage "https://github.com/yourusername/hello_world"
-  url "https://github.com/yourusername/hello_world/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "replace_this_with_the_sha256_of_your_tarball"
+  homepage "https://github.com/weilikai/hello_world"
+  url "https://raw.githubusercontent.com/weilikai/hello_world/9d68ea762529d53b375a34fb17a871a5aa8ecb20/hello_world"
+  sha256 "036e707ff7b4a823e1182fe6df81e90a93ae44ea17a8b2f0044a11cde169342e"
 
   def install
     bin.install "hello_world"
